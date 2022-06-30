35fb1924cce38cb70aefe04390ae8d82103d1845
Gilang Kurniawan <90315796+Gilangk1902@users.noreply.github.com>
2022-07-01 00:37:00 +0700

    shotgun

--- MyGame1FP/Assets/Scripts/shotGun.cs
+++ MyGame1FP/Assets/Scripts/shotGun.cs
@@ -7,24 +7,29 @@ public class shotGun : MonoBehaviour
     // Start is called before the first frame update
     public GameObject bullet;
     public Camera cam;
+    private Animator anim;
     RaycastHit hit;
 
     //STATS
     public float attackSpeed = 1.5f;
     bool wait = false;
+    int ammo = 2;
 
     //Enemy Script
     public status enemy;
     void Start()
     {
         enemy = GameObject.Find("Enemy").GetComponent<status>();
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.Mouse0)){
-            if(wait == false){
+        if(Input.GetKeyDown(KeyCode.Mouse0)){
+            if(wait == false && ammo != 0){
+                anim.Play("Armature|shoot", 0, 0.0f);
+                ammo--;
                 wait = true;
                 if(Physics.Raycast(cam.transform.position, cam.transform.forward + randomSpread(), out hit)){
                     Debug.Log("HIT");
@@ -102,7 +107,14 @@ public class shotGun : MonoBehaviour
                 
                 Invoke("waitNoMore", attackSpeed);
             }
+            else if(ammo == 0){
+                anim.Play("Armature|reloading", 0, 0.0f);
+                Invoke("reloading", 4f);
+            }
         }
+    }
+    void reloading(){
+        ammo = 2;
     }
     void waitNoMore(){
         wait = false;
